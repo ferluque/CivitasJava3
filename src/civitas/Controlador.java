6ec96d5821f5b1bcdaaa5f7092d95775d1a4bae5
Fernando Luque <e.fl1562001@go.ugr.es>
2020-11-02 20:07:20 +0100

    Declarad@s clases y enumerados

---
+++ src/civitas/Controlador.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package civitas;
+
+/**
+ *
+ * @author ferluque
+ */
+public class Controlador {
+    private CivitasJuego juego;
+    private VistaTextual vista;
+    
+    Controlador (CivitasJuego juego, VistaTextual vista) {
+        this.juego = juego;
+        this.vista = vista;
+    }
+    
+    void juega() {
+        
+    }
+}
